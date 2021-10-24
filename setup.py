8d4d06352b4aca0af1fe1819c3bbc9120c75e28f
abdala9512 <miguel.arquez12@gmail.com>
2021-10-23 22:26:33 -0500

    Adjust gh actions

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 setup(
     name='auto-mlu',
     packages=find_packages(include=['automlu']),
-    version='0.1.0',
+    version='0.2.0',
     description='Utilities for Machine learning development',
     author='Miguel Arquez Abdala',
     license='MIT',
