88ff2bf7e089189968653dbc05c7d707a801b675
abdala9512 <miguel.arquez12@gmail.com>
2021-10-23 20:09:19 -0500

    Lib Structure

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='auto-mlu',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Utilities for Machine learning development',
+    author='Miguel Arquez Abdala',
+    license='MIT',
+)
